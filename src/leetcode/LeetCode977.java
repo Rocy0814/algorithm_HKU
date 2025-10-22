fbe23bdf6a9e318740a3f5307f34c0c7269f6876
Rocy0814 <a160489358@gmail.com>
2025-10-22 16:43:22 +0800

    LeetCode27 & LeetCode977

---
+++ src/leetcode/LeetCode977.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LeetCode977 {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[nums.length];
+        int index = nums.length - 1;
+        while(index >= 0){
+            if(nums[left] * nums[left] >= nums[right] * nums[right]){
+                res[index--] = nums[left] * nums[left];
+                left++;
+            }else{
+                res[index--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
